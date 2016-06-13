bee1676115d6187fdd99f82015e9aeee30777a54
maxiunlm <maxiunlm@gmail.com>
2016-06-13 20:57:01 -0300

    Lost Scene
    Agregue el funcionamiento para perder las vidas

---
+++ SpriteKitPrueba1/GameLostScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameLostScene.swift
+//  SpriteKitPrueba1
+//
+//  Created by Maximiliano on 13/6/16.
+//  Copyright © 2016 Maximiliano. All rights reserved.
+//
+
+import Foundation
---
+++ SpriteKitPrueba1/GameWonScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameWonScene.swift
+//  SpriteKitPrueba1
+//
+//  Created by Maximiliano on 13/6/16.
+//  Copyright © 2016 Maximiliano. All rights reserved.
+//
+
+import Foundation
